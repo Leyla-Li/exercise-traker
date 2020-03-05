4ec85e3c8227cbe88d92283dccf1fa132db09311
leyla li <leylali@qq.com>
2020-03-05 15:08:38 -0800

    successfully connected to mongodb by mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors =require('cors');
+const mongoose = require('mongoose');
+const mongodb = require('mongodb');
 
 require('dotenv').config();
 
@@ -8,6 +10,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+   console.log('MongoDB connected successfully'); 
+})
+
 
 app.listen(port, ()=>{
     console.log(`Server is running on port: ${port}`);
