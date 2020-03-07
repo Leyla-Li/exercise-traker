baa5e31562c1111742aebcf1afd398cab7fb9c19
leyla li <leylali@qq.com>
2020-03-06 16:35:51 -0800

    add update route for exercise

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -37,9 +37,18 @@ router.route('/:id').delete((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/:id').delete((req, res) => {
-    Exercise.findByIdAndDelete(req.params.id)
-        .then(() => res.json('Excersice is deleted'))
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => {
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date);
+            
+            exercise.save()
+                .then(() => res.json('Updated Exercise'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
