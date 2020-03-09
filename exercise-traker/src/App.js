9c638b01b8c3fa263c72191759b5a1e424c55425
leyla li <leylali@qq.com>
2020-03-09 16:52:46 -0700

    create routes for different components

--- exercise-traker/src/App.js
+++ exercise-traker/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="container">
-      Hello World!
-    </div>
+    <Router>
+      <Navbar />
+      <br/>
+      <Route path='/' exact component={ExercisesList} />
+      <Route path='/edit/:id' exact component={EditExercise} />
+      <Route path='/create' exact component={CreateExercise} />
+      <Route path='/user' exact component={CreateUser} />
+    </Router>
   );
 }
 
