2e75fb8506f2f8b89d56cac0eea9f804e361bfec
leyla li <leylali@qq.com>
2020-03-11 15:47:10 -0700

    add submit button and add datepicker component in

--- exercise-traker/src/components/create-exercise.component.js
+++ exercise-traker/src/components/create-exercise.component.js
@@ -79,6 +79,39 @@ export default class CreateExercise extends Component {
 
                     <div className='form-group'>
                         <label>Description:</label>
+                        <input 
+                        type='text' 
+                        required 
+                        className='form-control' 
+                        value={this.state.description} 
+                        onChange={this.onChangeDescription}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label>Duration (in minutes):</label>
+                        <input 
+                        type='text' 
+                        required 
+                        className='form-control' 
+                        value={this.state.duration} 
+                        onChange={this.onChangeDuration}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label>Date:</label>
+                        <div>
+                            <DatePicker 
+                            selected={this.state.date}
+                            onChange={this.onChangeDate}
+                            />
+                        </div>
+                    </div>
+                    <div className='form-group'>
+                        <input 
+                        type='submit' 
+                        className='btn btn-primary' 
+                        value='Create a new exercise' 
+                        />
                     </div>
                 </form>
             </div>
