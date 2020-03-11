4a29a69c173169a9e7142d9abcdd9be0ab4cad9b
leyla li <leylali@qq.com>
2020-03-11 16:04:03 -0700

    create form to insert new user info

--- exercise-traker/src/components/create-user.component.js
+++ exercise-traker/src/components/create-user.component.js
@@ -31,7 +31,26 @@ export default class CreateUser extends Component {
     render() {
         return(
             <div>
-                <p>You are on the Create User Component</p>
+                <h3>Create A New User</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className='form-group'>
+                        <label>Username: </label>
+                        <input 
+                        type='text'
+                        required
+                        className='form-control'
+                        value={this.state.username}
+                        onChange={this.onChangeUsername}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input 
+                        type='submit'
+                        value='Create User'
+                        className='btn btn-primary'
+                        />
+                    </div>
+                </form>
             </div>
         );
     }
