fd8f46a047a2e8cc66c18185f2de961a06c6fe88
leyla li <leylali@qq.com>
2020-03-18 12:58:12 -0700

    file deploying to heroku need to reorganize files

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGOLAB_COPPER_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
 const connection = mongoose.connection;
@@ -25,7 +25,6 @@ const userRouter = require('./routes/users');
 
 app.use('/exercises', exerciseRouter);
 app.use('/users', userRouter);
-
 
 
 app.listen(port, ()=>{
